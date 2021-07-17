af9de11faecce40ccbb6103cd0099a97c5f62b1f
Nikita Popov <ningen.tos@gmail.com>
2021-07-17 18:42:22 +0500

    List of pets

--- src/app/bus/pet/index.js
+++ src/app/bus/pet/index.js
@@ -2,13 +2,15 @@
 import React from "react";
 
 // Components
-import {Counter} from "./counter";
+import { Counter } from "./counter";
+import { List } from "./list";
 
 export const Pet = () => {
   return (
     <>
       <h1>Pet</h1>
       <Counter/>
+      <List/>
     </>
   )
 }
