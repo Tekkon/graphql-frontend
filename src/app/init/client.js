b23cfbd50d3458f43bc2a829b76f42f16524206f
Nikita Popov <ningen.tos@gmail.com>
2021-07-18 14:29:56 +0500

    websocket

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,11 +1,15 @@
 // Core
 import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 
 // GraphQL Server
-const uri = 'https://funded-pet-library.moonhighway.com/';
+const root = 'funded-pet-library.moonhighway.com';
+const uri = `https://${root}`;
 const httpLink = createHttpLink({
   uri
 });
@@ -22,10 +26,30 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
+// WS
+const wsLink = new WebSocketLink({
+  uri: `wss://${root}/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
 // Cache initialization
 const cache = new InMemoryCache();
 
+const wrappedHttpLink = authLink.concat(httpLink);
+
+const link = split(({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  wrappedHttpLink)
+
 export const client = new ApolloClient({
   cache,
-  link: authLink.concat(httpLink)
+  link
 });
